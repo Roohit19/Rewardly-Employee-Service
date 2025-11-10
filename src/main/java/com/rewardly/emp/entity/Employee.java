76905b85700e5bab5445c9d7e9cf1dda54b1d710
waseem-sk-dev <shaikhrgp@gmail.com>
2025-11-10 17:25:52 +0530

    Implementd logs and javadoc in service

--- src/main/java/com/rewardly/emp/entity/Employee.java
+++ src/main/java/com/rewardly/emp/entity/Employee.java
@@ -4,8 +4,8 @@ import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
